cc00d02d98eccf2eb7c9ea6863e063443f5a0fea
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2018-12-13 12:55:49 -0700

    Initial commit.

---
+++ playground/mongodb-find.js
@@ -0,0 +1,39 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+let obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find({_id: new ObjectID('5c11d31f8ebf6952acccd064')}).toArray().then((docs) => {
+    //
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //
+    //     console.log('Todos');
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({name: "Jeremiah"}).toArray().then((docs) => {
+
+        console.log('Todos');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch todos', err);
+    });
+
+    //client.close();
+});
